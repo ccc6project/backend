c305079da2b1eff1fa0ead9d04e6baee4a0da740
cruzambrociogl <cruz.ambrocio@galileo.edu>
2025-10-06 23:31:53 -0600

    get due_date, cut_date validation removed

--- src/controllers/creditCard.controller.ts
+++ src/controllers/creditCard.controller.ts
@@ -37,9 +37,9 @@ export const issueCreditCard = async (req: Request, res: Response) => {
     // Validaciones formato
     if (expiration_date && !validateExpDate(expiration_date))
       return res.status(400).json({ error: "Formato de fecha de vencimiento incorrecto (yyyymm)" });
-    if (cut_date && !validateDateYMD(cut_date))
+    if (cut_date)
       return res.status(400).json({ error: "Formato de fecha de corte incorrecto (yyyymmdd)" });
-    if (due_date && !validateDateYMD(due_date))
+    if (due_date)
       return res.status(400).json({ error: "Formato de fecha de pago incorrecto (yyyymmdd)" });
     if (emisor_id && emisor_id.length !== 15)
       return res.status(400).json({ error: "El emisor debe tener 15 caracteres" });
